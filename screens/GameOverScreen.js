ebc3b881676e0096c970f4ee5eedae1661eba3e6
yeryshevArtem <artem.yeryshev@gmail.com>
2024-02-21 19:13:53 +0100

    Style GameOverScreen page

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 function GameOverScreen() {
     return (
-        <View>
-            <Text>Game is over</Text>
+        <View style={styles.rootContainer}>
+            <Title>GAME OVER</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../assets/images/success.png')} />
+            </View>
+            <View>
+                <Text>Your phone needed X rounds to guess the number Y.</Text>
+            </View>
         </View>
     );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        borderRadius: 150,
+        width: 300,
+        height: 300,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
