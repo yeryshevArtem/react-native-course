128a4ab02865485036a1dfba4edd57093aba305f
yeryshevArtem <artem.yeryshev@gmail.com>
2024-03-01 12:41:46 +0100

    Add passing the navigation parameters dynamically

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0;
     });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+        navigation.setOptions({
+            title: categoryTitle
+        });
+    }, [catId, navigation]);
 
     function renderMealItem(itemData) {
         const mealItemProps = {
