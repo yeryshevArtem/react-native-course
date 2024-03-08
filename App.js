3dbcfe416f4473cd4237008ae57279a1eb15c213
yeryshevArtem <artem.yeryshev@gmail.com>
2024-03-08 13:11:14 +0100

    Add nested navigation

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator 
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#351401',
+        },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#3f2f25'
+        }
+      }}
+    >
+      <Drawer.Screen name="Categories" component={CategoriesScreen} options={{ title: "All Categories"}} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -24,21 +46,15 @@ export default function App() {
           }}
         >
           <Stack.Screen 
-            name="MealsCategories" 
-            component={CategoriesScreen} 
-            options={{
-              title: 'All Categories'
+            name="Drawer" 
+            component={DrawerNavigator} 
+            options={{ 
+              headerShown: false
             }}
           />
           <Stack.Screen 
             name="MealsOverview" 
             component={MealsOverviewScreen}
-            options={({ route, navigation }) => {
-              const catId = route.params.categoryId;
-              return {
-                title: catId
-              };
-            }}
           />
           <Stack.Screen 
             name="MealDetail"
