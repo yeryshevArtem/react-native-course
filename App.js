178c4eab67f601f8fca8ad4c772e88b0edf8b3bd
yeryshevArtem <artem.yeryshev@gmail.com>
2024-03-11 18:35:08 +0100

    Add react context for favorite meals

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
 import { Ionicons } from "@expo/vector-icons";
+import FavoritesContextProvider from './store/context/favorites-context';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -47,35 +48,37 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#351401',
-            },
-            headerTintColor: 'white',
-            contentStyle: {
-              backgroundColor: '#3f2f25'
-            }
-          }}
-        >
-          <Stack.Screen 
-            name="Drawer" 
-            component={DrawerNavigator} 
-            options={{ 
-              headerShown: false
+      <FavoritesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#351401',
+              },
+              headerTintColor: 'white',
+              contentStyle: {
+                backgroundColor: '#3f2f25'
+              }
             }}
-          />
-          <Stack.Screen 
-            name="MealsOverview" 
-            component={MealsOverviewScreen}
-          />
-          <Stack.Screen 
-            name="MealDetail"
-            component={MealDetailScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+          >
+            <Stack.Screen 
+              name="Drawer" 
+              component={DrawerNavigator} 
+              options={{ 
+                headerShown: false
+              }}
+            />
+            <Stack.Screen 
+              name="MealsOverview" 
+              component={MealsOverviewScreen}
+            />
+            <Stack.Screen 
+              name="MealDetail"
+              component={MealDetailScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
     </>
   );
 }
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,26 +1,42 @@
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useContext } from 'react';
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
 import IconButton from '../components/IconButton';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 function MealDetailScreen({ route, navigation }) {
+
+    const favoriteMealsCtx = useContext(FavoritesContext);
 
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-    function headerButtonPressHandler() {
-        console.log('Pressed')
+    const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+    function changeFavoriteStatusHandler() {
+        if (mealIsFavorite) {
+            favoriteMealsCtx.removeFavorite(mealId);
+        } else {
+            favoriteMealsCtx.addFavorite(mealId);
+        }
     }
+
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => {
-                return <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />
+                return (
+                    <IconButton 
+                        icon={mealIsFavorite ? "star" : "star-outline"}
+                        color="white" 
+                        onPress={changeFavoriteStatusHandler} 
+                    />
+                );
             }
         });
-    }, [navigation, headerButtonPressHandler]);
+    }, [navigation, changeFavoriteStatusHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
