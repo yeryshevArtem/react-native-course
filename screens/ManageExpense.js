4d5978ba88eb352327a5f49d0e0ed99a10e331c1
yeryshevArtem <artem.yeryshev@gmail.com>
2024-03-18 19:36:17 +0100

    Add custom button

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
 
 function ManageExpense({ route, navigation }) {
     const editedExpenseId = route.params?.expenseId;
@@ -18,8 +19,20 @@ function ManageExpense({ route, navigation }) {
 
     }
 
+    function cancelHandler() {
+            
+    }
+
+    function confirmHandler() {
+
+    }
+
     return (
         <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button mode="flat" onPress={cancelHandler} style={styles.button}>Cancel</Button>
+                <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
             { isEditing && (
                 <View style={styles.deleteContainer}>
                     <IconButton 
@@ -48,5 +61,15 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
         backgroundColor: GlobalStyles.colors.primary800
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8
     }
+
 })
